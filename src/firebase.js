1ea62d0b5a816bf9fcb0387c25b0729038641c28
Kian Hennessy <66495302+kianhennessy@users.noreply.github.com>
2023-02-21 18:54:59 +0000

    firebase connection added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+
+export const auth = firebase.initializeApp({
+    apiKey: "AIzaSyD2O1330XlDbu9ovXlqLOUy5aIct0ZB46I",
+    authDomain: "project-57eb2.firebaseapp.com",
+    projectId: "project-57eb2",
+    storageBucket: "project-57eb2.appspot.com",
+    messagingSenderId: "208101619513",
+    appId: "1:208101619513:web:076172c575b2ba9d5edae8",
+    measurementId: "G-3R3PG5W4CF"
+}).auth();
